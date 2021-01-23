76a744e21532ed2ae41bfcfc6d88b5a05c526c0e
VinsonWang92 <vinsonwang92@gmail.com>
2021-01-23 16:45:29 +1100

    set up completed

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const Header = (props) => {
+    const onClick = () => {
+        console.log('click')
+    }
+    
+    return (
+        <header className='header'>
+            <h1>{props.title}</h1>
+            <Button color='green' text='Hello' onClick={onClick} />
+        </header>
+            
+    )
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+export default Header
