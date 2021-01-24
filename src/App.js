4f5983d4bd0d8a36b6c73e3dfd9391d3ca37ca73
VinsonWang92 <vinsonwang92@gmail.com>
2021-01-24 22:09:53 +1100

    fixed errors by putting data in app JS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-
+import { useState } from 'react'
 import './App.css';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState( [
+    {
+      id:1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    }
+  ])
   return (
     <div className="container">
       <Header title ='Task Tracker'/>
-      <Tasks />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
+
+// Delete Task
+const deleteTask = (id) => {
+  console.log('delete', id)
+}
+
 
 export default App;
